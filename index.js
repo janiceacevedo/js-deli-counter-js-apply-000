d4745da1376e2617bbe4f9a36bcc46e808ba9685
blithe-debugger-5623 <janiceacevedo1@gmail.com>
2020-09-27 02:31:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,25 +2,27 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return "Welcome," + (name) + "You are number" + (katzDeliLine) + "in line.";
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
 }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
   return "There is nobody waiting to be served!";
   }
-  if (katzDeliLine > 0) {
-  return "Now serving" + katzDeliLine.push(name);
+  if (katzDeliLine.length > 0) {
+  var customer = katzDeliLine.shift()
+  return "Currently serving " + customer + "." ;
   }
-  else
-  "Currently serving" + katzDeliLine.push(name); ".";
 }
 
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
+  } else {
+  var newArray = [];
+  for (var i = 0; i < katzDeliLine.length; i++) {
+    newArray.push([i + 1] + ". " + katzDeliLine[i]);
   }
-  for (var i = 0; i < katzDeliLine.length; i++)
-    katzDeliLine.push(i + 1 + ". " +katzDeliLine);
-  return "The line is currently:" + push(name);
+  return "The line is currently: " + newArray.join(", ");
   }
+}
